957b66b4ba32d55f599fead3f7445e6eabaca1b6
Tavita <menashetavita@gmail.com>
2023-05-21 20:01:36 +0600

    feat: add Navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-function App() {
-  return (
-    <div>
-        3d portfolio
-    </div>
-  )
-}
+import { BrowserRouter } from "react-router-dom";
 
-export default App
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
